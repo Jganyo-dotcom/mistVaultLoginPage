a5d63e57596a49a71eb05357dac8d96ffcd6fe75
James Ganyo <elikemejay@gmail.com>
2026-06-05 13:45:26 +0000

    field mismatch

--- app.js
+++ app.js
@@ -161,7 +161,7 @@
       const response = await fetch('https://medsec.onrender.com/api/login-hospital', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ identifier, password })
+        body: JSON.stringify({ identifier :email, password })
       });
 
       const data = await response.json();
